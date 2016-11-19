b9dca5e45166fe5947ab71107611f06d961c7742
https://github.com/thesixteenth/MoneyHo <khh1810@naver.com>
2016-11-19 19:23:04 +0900

    [modified]
    입금 출금 버튼 추가
    InputText추가
    DB Connect 추가

---
+++ MoneyHo/MoneyHo/MoneyIOViewControl.swift
@@ -0,0 +1,15 @@
+//
+//  MoneyIOViewControl.swift
+//  MoneyHo
+//
+//  Created by kwonHyunHo on 2016. 11. 19..
+//  Copyright © 2016년 solsol. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+class MoneyViewController : UIViewController
+{
+    
+}
