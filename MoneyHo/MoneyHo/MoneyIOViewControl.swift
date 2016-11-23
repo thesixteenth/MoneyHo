8813e4413cc57acc157ffc26ab5a692abb636ff1
https://github.com/thesixteenth/MoneyHo <khh1810@naver.com>
2016-11-23 15:28:57 +0900

    [added]
    - 현재 날짜의 입금 금액들 추가

--- MoneyHo/MoneyHo/MoneyIOViewControl.swift
+++ MoneyHo/MoneyHo/MoneyIOViewControl.swift
@@ -8,19 +8,19 @@
 
 import Foundation
 import UIKit
- var databasePath = NSString()
+var databasePath = NSString()
 
 class MoneyViewController : UIViewController
 {
     @IBOutlet weak var currentMoney: UITextField!
     
-   
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         let filemgr = FileManager.default
         let dirPaths = filemgr.urls(for: .documentDirectory, in: .userDomainMask)
         
-        databasePath = dirPaths[0].appendingPathComponent("MoneyHO.db").path as NSString
+        databasePath = dirPaths[0].appendingPathComponent("MoneyHOS.db").path as NSString
         
         
         if !filemgr.fileExists(atPath: databasePath as String) {
@@ -32,13 +32,18 @@ class MoneyViewController : UIViewController
             }
             
             if (contactDB?.open())! {
-                let sql_stmt = "CREATE TABLE IF NOT EXISTS MONEYHO (ID INTEGER PRIMARY KEY AUTOINCREMENT, BALANCE TEXT, DEPOSIT TEXT, WITHDRAW TEXT, TOTALDEPOSIT TEXT, TOTALWITHDRAW TEXT)"
+                let sql_stmt = "CREATE TABLE IF NOT EXISTS MONEYHOS (ID INTEGER PRIMARY KEY AUTOINCREMENT, BALANCE TEXT, DEPOSIT TEXT, WITHDRAW TEXT, TOTALDEPOSIT TEXT, TOTALWITHDRAW TEXT, DATA TEXT)"
                 if !(contactDB?.executeStatements(sql_stmt))! {
                     print("Error ####: \(contactDB?.lastErrorMessage())")
                 }
                 
                 //Table 생성 이후에 초기값으로 0 을 세팅 한다.
-                let insertSQL = "INSERT INTO CONTACTS (BALANCE, DEPOSIT, WITHDRAW,TOTALDEPOSIT,TOTALWITHDRAW) VALUES ('0', '0', '0', '0', '0')"
+                let todaysDate = NSDate()
+                let dateFormatter = DateFormatter()
+                dateFormatter.dateFormat = "yyyyMMdd"
+                let DateInFormat = dateFormatter.string(from: todaysDate as Date)
+                
+                let insertSQL = "INSERT INTO MONEYHOS (BALANCE, DEPOSIT, WITHDRAW,TOTALDEPOSIT,TOTALWITHDRAW,DATA) VALUES ('0', '0', '0', '0', '0', '\(DateInFormat)')"
                 
                 contactDB?.executeUpdate(insertSQL,withArgumentsIn: nil)
                 contactDB?.close()
@@ -52,7 +57,7 @@ class MoneyViewController : UIViewController
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
+    
     @IBAction func deposit(_ sender: Any)
     {
         // 기존에 입금된 금액이랑 지금 입금된 금액을 더해서 DB에 넣어야 된다. (balance 값도 변경)
@@ -66,7 +71,7 @@ class MoneyViewController : UIViewController
             var totalDeposit = String()
             
             // 잔액 및 전체 입금 금액 찾아오기  ID는 1 번 값만 사용한다.
-            let querySQL = "SELECT BALANCE, TOTALDEPOSIT FROM MONEYHO WHERE ID = 1"
+            let querySQL = "SELECT BALANCE, TOTALDEPOSIT FROM MONEYHOS WHERE ID = 1"
             
             let results:FMResultSet? = contactDB?.executeQuery(querySQL,
                                                                withArgumentsIn: nil)
@@ -91,7 +96,7 @@ class MoneyViewController : UIViewController
                 let convertTotalDeposit : Int = Int(totalDeposit)!
                 let insertTotalDeposit : Int = convertTotalDeposit + convertCurrentMoney
                 
-                let insertSQL = "UPDATE MONEYHO SET BALANCE = '\(String(insertBalance))',TOTALDEPOSIT = '\(String(insertTotalDeposit))' WHERE ID = 1"
+                let insertSQL = "UPDATE MONEYHOS SET BALANCE = '\(String(insertBalance))',TOTALDEPOSIT = '\(String(insertTotalDeposit))' WHERE ID = 1"
                 
                 
                 // 현재 입금 금액을 전체 입금 금액이랑 잔액에 더한 값을 넣는다.
@@ -110,7 +115,46 @@ class MoneyViewController : UIViewController
             print("Error @@@: \(contactDB?.lastErrorMessage())")
             
         }
-         
+        
+        //날자 별로 입금 금액을 전부 기억 하고 있어야 한다
+        let todaysDate = NSDate()
+        let dateFormatter = DateFormatter()
+        dateFormatter.dateFormat = "yyyyMMdd"
+        let DateInFormat = dateFormatter.string(from: todaysDate as Date)
+        
+        var depositMoney = String()
+        print(DateInFormat)
+        
+        if (contactDB?.open())! {
+            
+            // 현재 날자에 입금된 모든 금액을 가져 온다
+            let querySQL = "SELECT DEPOSIT FROM MONEYHOS WHERE DATA = '\(DateInFormat)'"
+            
+            let results:FMResultSet? = contactDB?.executeQuery(querySQL,
+                                                               withArgumentsIn: nil)
+            
+            if results?.next() == true {
+                //입금된 금액에 현재 금액을 더한 값을 Insert한다
+                depositMoney = (results?.string(forColumn: "DEPOSIT"))!
+                depositMoney += "," + "\(currentMoney.text!)"
+            }
+            else {
+                print("Fail")
+            }
+            
+            print(depositMoney)
+            let insertSQL = "UPDATE MONEYHOS SET DEPOSIT = '\(depositMoney)' WHERE DATA= '\(DateInFormat)'"
+            
+            let result = contactDB?.executeUpdate(insertSQL,
+                                                  withArgumentsIn: nil)
+            
+            if !result! {
+                print("Error: \(contactDB?.lastErrorMessage())")
+            }
+            else {
+            }
+        }
+        
     }
     @IBAction func withdraw(_ sender: UIButton)
     {
@@ -139,7 +183,7 @@ class MoneyViewController : UIViewController
             else {
                 print("Fail")
             }
- 
+            
             if currentMoney.text! != ""
             {
                 let convertBalance : Int = Int(currentBalance)!
@@ -173,8 +217,8 @@ class MoneyViewController : UIViewController
             print("Error @@@: \(contactDB?.lastErrorMessage())")
             
         }
-
-
+        
+        
     }
     @IBAction func closeView(_ sender: UIButton)
     {
