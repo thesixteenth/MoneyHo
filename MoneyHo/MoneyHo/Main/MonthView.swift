a03c265facf9ffa7ba258691c781bf0802602a6e
solvely <p.solvely@gmail.com>
2016-11-23 22:44:52 +0900

    [added]
    - MonthView swift 파일과 xib 파일 생성

---
+++ MoneyHo/MoneyHo/Main/MonthView.swift
@@ -0,0 +1,21 @@
+//
+//  MonthView.swift
+//  MoneyHo
+//
+//  Created by sol on 2016. 11. 23..
+//  Copyright © 2016년 solsol. All rights reserved.
+//
+
+import UIKit
+
+class MonthView: UIView {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
